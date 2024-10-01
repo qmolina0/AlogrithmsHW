f6e7de9e2237e345813784b002e78a0682f7286e
qmolina0 <qmolina@terpmail.umd.edu>
2024-10-01 19:23:22 -0400

    Add files via upload

---
+++ AlgorithmsHw2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 13:37:45 2024
+
+@author: quentin
+"""
+def merge_sorted_arrays(arr1 , arr2):
+    sort = []
+    count1 = 0
+    count2 = 0
+    
+    while count1 != len(arr1) and count2 != len(arr2): 
+        
+        if arr1[count1] < arr2[count2]:
+            sort.append(arr1[count1])
+            count1 += 1
+            
+        elif arr1[count1] > arr2[count2]:
+            sort.append(arr2[count2])
+            count2 += 1
+            
+        elif arr1[count1] == arr2[count2]:
+            sort.append(arr1[count1])
+            sort.append(arr2[count2])
+            count1 += 1
+            count2 += 1
+            
+        if count1 >= len(arr1):
+            for i in range(count2, len(arr2)):
+                sort.append(arr2[i])
+        if count2 >= len(arr2):
+            for i in range(count1, len(arr1)):
+                sort.append(arr1[i])
+            
+    return sort
+    
+    
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+
+print(merge_sorted_arrays(arr1, arr2))
+
+# function from https://www.codecademy.com/learn/sorting-algorithms-python/modules/merge-sort-python/cheatsheet
+def merge_sort(lst):
+  if len(lst) <= 1:
+    return lst
+  middle = len(lst) // 2
+  left = lst[:middle]
+  right = lst[middle:]
+  sleft = merge_sort(left)
+  sright = merge_sort(right)
+  return merge_sorted_arrays(sleft, sright)
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+arr3 = merge_sort(arr)
+print(merge_sort(arr3))
+
+
+
