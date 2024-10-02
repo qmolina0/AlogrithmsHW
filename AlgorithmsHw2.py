73df2a9667e957536309c9772b0dd63dcfdff1b3
qmolina0 <qmolina@terpmail.umd.edu>
2024-10-02 17:32:27 -0400

    Update AlgorithmsHw2.py

--- AlgorithmsHw2.py
+++ AlgorithmsHw2.py
@@ -34,14 +34,7 @@ def merge_sorted_arrays(arr1 , arr2):
                 sort.append(arr1[i])
             
     return sort
-    
-    
-arr1 = [1, 3, 5]
-arr2 = [2, 4, 6]
 
-print(merge_sorted_arrays(arr1, arr2))
-
-# function from https://www.codecademy.com/learn/sorting-algorithms-python/modules/merge-sort-python/cheatsheet
 def merge_sort(lst):
   if len(lst) <= 1:
     return lst
@@ -51,10 +44,20 @@ def merge_sort(lst):
   sleft = merge_sort(left)
   sright = merge_sort(right)
   return merge_sorted_arrays(sleft, sright)
+    
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+
+print(merge_sorted_arrays(arr1, arr2))
+
+# function from https://www.codecademy.com/learn/sorting-algorithms-python/modules/merge-sort-python/cheatsheet
+
 
 arr = [38, 27, 43, 3, 9, 82, 10]
-arr3 = merge_sort(arr)
-print(merge_sort(arr3))
+arr2 = [54, 34, 97, 45, 234, 0]
+
+print(merge_sort(arr))
+print(merge_sort(arr2))
 
 
 
